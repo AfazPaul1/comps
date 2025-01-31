e6730db5f4fcbf03e5ad0f96c5df8f3ff5bcc902
AfazPaul1 <afazpaul@gmail.com>
2025-01-31 16:42:52 +0530

    conditional styling using classnames library

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import Button1 from "./Button";
 function App() {
     return (
         <div>
-            <Button1 primary >click me</Button1>
-            <Button1>Buy now</Button1>
-            <Button1>see deals</Button1>
-            <Button1>hide ads</Button1>
-            <Button1>something</Button1>
+            <Button1 primary>click me</Button1>
+            <Button1 secondary>Buy now</Button1>
+            <Button1 success>see deals</Button1>
+            <Button1 warning outline>hide ads</Button1>
+            <Button1 danger rounded>something</Button1>
         </div>
     )
 }
