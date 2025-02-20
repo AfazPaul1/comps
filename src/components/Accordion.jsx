24ea8a1a5f2791d908ba003bf5158df18c0ab6c3
AfazPaul1 <afazpaul@gmail.com>
2025-02-20 15:25:20 +0530

    styling and toggling items

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,34 +2,48 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-
+import '../main.css'
 
 
 function Accordion({ items }) {
 
-    const [expandedIndex, setExpandedIndex] = useState(0)
+    const [expandedIndex, setExpandedIndex] = useState(-1)
 
-    const handleClick = (nextIndex) => setExpandedIndex(nextIndex)
+    const handleClick = (nextIndex) => {
+        console.log(expandedIndex);
+        
+        if(expandedIndex === nextIndex) {
+            setExpandedIndex(-1)
+        }
+        else {
+            setExpandedIndex(nextIndex)
+        }
+    }
 
     const renderedList = items.map((item, index) => {
 
         const isExpanded = index === expandedIndex
 
-        const content = isExpanded && <div>{item.content}</div>
+        const content = isExpanded && <div className="border-b p-3">{item.content}</div>
 
         const icon = <span>{isExpanded? <ArrowDropDownIcon /> : <ArrowRightIcon/>}</span>
             
         return (
                 <div key={index}>
-                    {icon}
-                    <div onClick={() => handleClick(index)}>{item.label}</div> 
-                    { content }
+                    <div onClick={() => handleClick(index)} className="flex items-center bg-gray-50 border-b p-3">
+                        {icon}
+                        <div>{item.label}</div> 
+                    </div>
+                    <div >
+                        { content }
+                    </div>
+                    
                 </div> 
             ) 
     })
 
     return (
-        <div>
+        <div className="border-x border-t rounded ">
             {renderedList}
         </div>
     )
