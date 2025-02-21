c44b7342c644b4fc4cd004edd082fb144785bb34
AfazPaul1 <afazpaul@gmail.com>
2025-02-21 20:13:23 +0530

    placed a click handler on the dropdown to toggle isOpen to toggle dropdown items to be shown and hidden

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 
 function Dropdown({options}) {
 
-    const [isOpen, setIsOpen] = useState(true)
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleClick = () => {
+        //we learnt that if the new state is dependent on the old state we use a updater function
+        //but since it is only required is a user click at a very rapid pace it is ususally not required
+        //setIsOpen((currentIsOpen) => !currentIsOpen)
+        setIsOpen(!isOpen)
+    }
 
 
     const renderedOptions = isOpen && options.map((option) => {
@@ -14,7 +21,7 @@ function Dropdown({options}) {
 
     return (
         <div>
-            <div>Selected..</div> 
+            <div onClick = {handleClick}>Selected..</div> 
             <div>{renderedOptions}</div>
         </div>
     )
