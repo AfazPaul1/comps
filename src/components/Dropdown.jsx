f63d98d19fd890e9e7ca1cbc5783028ab8ed5184
AfazPaul1 <afazpaul@gmail.com>
2025-02-22 13:00:46 +0530

    wrote a hybrid approach to eventhandler by moving the eventhandler outside the map (avoids multiple creations of same function) and giving it access to the clicked option by running the event handler with the option as the argument and wrapping it in another function and passing that to onclick

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -6,15 +6,18 @@ function Dropdown({options}) {
     const [isOpen, setIsOpen] = useState(false)
 
     const handleClick = () => {
-        //we learnt that if the new state is dependent on the old state we use a updater function
-        //but since it is only required is a user click at a very rapid pace it is ususally not required
-        //setIsOpen((currentIsOpen) => !currentIsOpen)
         setIsOpen(!isOpen)
+    }
+
+    const handleItemClick = (option) => {
+        console.log(option);
+        
+        setIsOpen(false)
     }
 
 
     const renderedOptions = isOpen && options.map((option) => {
-        return <div key={option.value}>{option.label}</div>
+        return <div onClick={() => handleItemClick(option.value)} key={option.value}>{option.label}</div>
     })
 
 
