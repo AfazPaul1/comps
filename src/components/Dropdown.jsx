384eaf10b36def9985b711b5e0e9e9ddd93eb8db
AfazPaul1 <afazpaul@gmail.com>
2025-03-04 14:50:06 +0530

    created a panel component with some styling to have consistent and reusable styling

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../main.css'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
+import Panel from './Panel';
 
 function Dropdown({options, onChange, value}) {
 
@@ -24,14 +24,14 @@ function Dropdown({options, onChange, value}) {
 
     return (
         <div className='w-48 relative'>
-            <div 
-                className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-ful' 
+            <Panel 
+                className='flex justify-between items-center cursor-pointer' 
                 onClick = {handleClick}
             >
                 {value?.label || "Select.."}
                 <ExpandMoreIcon />
-            </div> 
-            { isOpen && <div className='absolute top-full border rounded p-3 shadow bg-white w-full'>{renderedOptions}</div>}
+            </Panel> 
+            { isOpen && <Panel className='absolute top-full'>{renderedOptions}</Panel>}
         </div>
     )
 
