7726312955dc44dc25b67d91d5e5dabef87f4538
AfazPaul1 <afazpaul@gmail.com>
2025-03-11 00:14:40 +0530

    `feat: Implement custom navigation with Link and Route components using context`
    - Added `Link` component that renders an anchor (`<a>`) and prevents default navigation while calling `navigate(to)` from context.
    - Added `Route` component that consumes `currentPath` from context and conditionally renders children if the path matches.
    - Integrated `Link` and `Route` to handle client-side navigation for `/accordion` and `/dropdown` routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-
+import Link from "./components/link";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
 function App() {
 
-    return <div></div>
+    return <div>
+        <Link to='/accordion'>Go to accordion</Link>
+        <Link to='/dropdown'>Go to dropdown</Link>
+        <Route path="/accordion">
+            <AccordionPage></AccordionPage>
+        </Route>
+        <Route path="/dropdown">
+            <DropdownPage  />
+        </Route>
+    </div>
 }
 
 export default App;
