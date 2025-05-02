fd5d3eb2460e888a2613f3bcb5cc989abd20ac71
AfazPaul1 <afazpaul@gmail.com>
2025-05-02 21:16:38 +0530

    implemented logic to decrease count and added a controlled input which takes in the input value and increases count by that value on the add button click. used mui elements

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,21 +1,43 @@
 import { useState, useEffect } from "react";
 import Button1 from "../components/Button";
-
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Panel from '../components/Panel'
 function CounterPage( {initialCounter} ) {
-    const [counter, setCounter] = useState(initialCounter)
+    const [count, setCounter] = useState(initialCounter)
+    const [incrementBy, setIncrementBy] = useState()
 
-    const handleClick = () => {
-        setCounter(counter + 1)
+    const handleChange = (event) => {
+        console.log(typeof(event.target.value));
+        
+        setIncrementBy(Number(event.target.value))
+    }
+
+    const increment = () => {
+        setCounter(count + 1)
+    }
+    const decrement = () => {
+        setCounter(count - 1)
+    }
+
+    const add = () => {
+        setCounter(count + incrementBy)
     }
 
     useEffect(() => {
-        console.log(counter);
-    }, [counter])
+        console.log(count);
+    }, [count])
 
     return (
         <div>
-            {counter}
-            <Button1 onClick={handleClick} primary >Increment</Button1>
+            {count}
+            <Button1 onClick={increment} primary >Increment</Button1>
+            <Button1 onClick={decrement} primary >Deccrement</Button1>
+            <Box sx = {{display: 'flex', alignItems:'center', gap:'2', }}          component="form">
+                <TextField onChange={handleChange}  sx={{m:1}} id="outlined-number" label="Add a lot" variant="outlined" type="number" value={incrementBy} />
+                <Button onClick={add}  sx={{m:1}} variant="outlined">ADD</Button>
+            </Box>
         </div>
     )
 }
