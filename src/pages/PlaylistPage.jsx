1a56fc0eef19d1bb72319809dc0746a0d0fdcf4e
AfazPaul1 <afazpaul@gmail.com>
2025-05-06 15:43:37 +0530

    dynamic import bulma only for playlist page

--- src/pages/PlaylistPage.jsx
+++ src/pages/PlaylistPage.jsx
@@ -1,11 +1,25 @@
+
 import MoviePlaylist from "../components/MoviePlaylist";
 import SongPlaylist from "../components/SongPlaylist";
 import "../store"
 import {reset} from '../store'
 import { useDispatch } from "react-redux";
 import "../main.css";
+import { useEffect } from "react";
 function PlaylistPage () {
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+    document.head.appendChild(link);
+
+    return () => {
+      document.head.removeChild(link); // Clean up when navigating away
+    };
+  }, []);
+
   const handleClick = () => {
     dispatch(reset())
   }
